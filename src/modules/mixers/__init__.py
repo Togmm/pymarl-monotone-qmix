6adf4033f93c3bd9b5dc03edb49e69fdc2e7ac88
Togmm <Togmm@users.noreply.github.com>
2026-06-15 21:59:18 +0800

    1

--- src/modules/mixers/__init__.py
+++ src/modules/mixers/__init__.py
@@ -1,4 +1,7 @@
 from modules.mixers.amco_monotone import AMCOMonotoneMixer
+from modules.mixers.hll_monotone import HLLMonotoneMixer
+from modules.mixers.lmn_monotone import LMNMonotoneMixer
+from modules.mixers.monokan_monotone import MonoKANMonotoneMixer
 from modules.mixers.qmix import QMixer
 from modules.mixers.smm_monotone import SMMMonotoneMixer
 from modules.mixers.smnn_monotone import SMNNMonotoneMixer
@@ -10,5 +13,8 @@ REGISTRY = {}
 REGISTRY["vdn"] = lambda args: VDNMixer()
 REGISTRY["qmix"] = QMixer
 REGISTRY["amco"] = AMCOMonotoneMixer
+REGISTRY["hll"] = HLLMonotoneMixer
+REGISTRY["lmn"] = LMNMonotoneMixer
+REGISTRY["monokan"] = MonoKANMonotoneMixer
 REGISTRY["smm"] = SMMMonotoneMixer
 REGISTRY["smnn"] = SMNNMonotoneMixer
